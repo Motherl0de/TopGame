d1755e096c65e803b0e1a00073cb9a77a1c298ac
RobertEdHouse <robert.edwin.house101@gmail.com>
2023-10-08 18:04:41 +0300

    Fixed Water

--- Assets/Test Interaction System/Scripts/PlayerMovement.cs
+++ Assets/Test Interaction System/Scripts/PlayerMovement.cs
@@ -12,27 +12,23 @@ public class PlayerMovement : MonoBehaviour
     {
 
     }
-
-    // Update is called once per frame
     void Update()
     {
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
+        if (deltaX == 0 && deltaZ == 0)
+            return; 
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
         movement = Vector3.ClampMagnitude(movement, speed);
         movement *= Time.deltaTime;
         movement = transform.TransformDirection(movement);
-        //_characterRigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime), ForceMode.Impulse);
-        //_characterRigidbody.AddForce(transform.forward * speed);
 
 
         Vector3 targetVelocity = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))) * speed;
 
-        // Apply a force that attempts to reach our target velocity
         Vector3 velocity = _characterRigidbody.velocity;
         Vector3 velocityChange = (targetVelocity - velocity);
 
         _characterRigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
-        //transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime));
     }
 }
