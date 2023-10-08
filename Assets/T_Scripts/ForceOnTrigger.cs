694d482f90701f82f6598736d0bfc5ecefb36df9
Anastasia <timmikvo@gmail.com>
2023-10-08 12:49:49 +0300

    Add script for Water

--- Assets/T_Scripts/ForceOnTrigger.cs
+++ Assets/T_Scripts/ForceOnTrigger.cs
@@ -8,10 +8,11 @@ namespace T_Scripts
         private void OnTriggerStay(Collider other)
         {
             other.attachedRigidbody.AddForce(Vector3.up * 14.4f);
+
         }
 
         // private void OnCollisionStay(Collision other)
-        // {
+        // {Y = sin(k*t) * amplitude + water_depth
         //     if (other.gameObject.CompareTag("Player"))
         //     {
         //         other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 75f);
