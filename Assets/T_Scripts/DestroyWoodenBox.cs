b03e7a05cfd33b056a23375ef434a1125eb4c91f
Ivan <frey.terry@gmail.com>
2023-10-19 14:54:30 +0300

    Revert "Feature/effects box"

--- Assets/T_Scripts/DestroyWoodenBox.cs
+++ Assets/T_Scripts/DestroyWoodenBox.cs
@@ -1,49 +1,35 @@
 
-using System;
 using UnityEngine;
 
 namespace T_Scripts
 {
     public class DestroyWoodenBox : MonoBehaviour
     {
-        [SerializeField] private GameObject[] _box;
-        [SerializeField] private GameObject _partical;
-        [SerializeField] private Transform _spawn;
-        private GameObject _current;
-        private GameObject _next;
-        private int _pendingIndex;
+        [SerializeField]private GameObject[] _box;
+        GameObject _current;
+        GameObject _next;
+        int _index;
 
-        private void Awake()
+        void Awake()
         {
             _current = _box[0];
             _next = _box[1];
-            _pendingIndex = 2;
+            _index = 2;
         }
 
-        private void Update()
+        void Update()
         {
-            BreakOnClick();
-        }
-
-        private void OnCollisionStay(Collision other)
-        {
-            BreakOnClick();
-        }
-
-        private void BreakOnClick()
-        {
-            if (Input.GetMouseButtonDown(0) && _pendingIndex <= _box.Length)
+            //It's code must be in OnColliderStay()
+            if (Input.GetMouseButtonDown(0) && _index <= _box.Length)
             {
                 _current.SetActive(false);
                 if (_next is not null)
                 {
                     _next.SetActive(true);
-                    Instantiate(_partical, _spawn);
                 }
-
                 _current = _next;
-                _next = _pendingIndex < _box.Length ? _box[_pendingIndex] : null;
-                _pendingIndex++;
+                _next = _index < _box.Length ? _box[_index] : null;
+                _index++;
             }
         }
     }
