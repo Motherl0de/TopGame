4326708df3938326d0ae42226a56cd80f43cdd41
Alexandr Leto <Alexiell013@gmail.com>
2023-11-09 22:32:39 +0200

    add system take away HP over time

--- Assets/Scripts/UI/Health.cs
+++ Assets/Scripts/UI/Health.cs
@@ -7,24 +7,59 @@ using UnityEngine.UIElements;
 using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class Health : MonoBehaviour
-        
 {
-
     public GameObject healthFill;
-    public int _health;
 
-    public int MaxHealth;
+    public int startingHP = 100;
 
-    public void TakeDamag(int Damag)
+    public int currentHP;
+
+    public int MaxHealth ;
+
+    private float hpDecrementInterval = 1.0f;
+
+    private float timer;
+    private void DecrementHP(int amount)
     {
-        _health -= Damag;
+        currentHP -= amount;
 
-        healthFill.GetComponent<UnityEngine.UI.Image>().fillAmount = (float)(_health / 100f);
+        //Debug.Log("HP: " + currentHP);
 
+        healthFill.GetComponent<UnityEngine.UI.Image>().fillAmount = currentHP/100f;
 
-        if (_health <= 0)
+        if (currentHP <= 0)
         {
-            Destroy(gameObject);
+            // Здесь можно добавить дополнительные действия при достижении HP <= 0
+            Debug.Log("Game Over");
+        }
+    }
+
+    //public void TakeDamag(int Damag)
+    //{
+    //    currentHP -= Damag;
+
+    //    healthFill.GetComponent<UnityEngine.UI.Image>().fillAmount = currentHP / 100f;
+
+    //    if (currentHP <= 0)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+    private void Start()
+    {
+        currentHP = startingHP;
+
+        timer = hpDecrementInterval;
+    }
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            DecrementHP(1); // Отнимаем 1 HP
+
+            timer = hpDecrementInterval; // Сбрасываем таймер
         }
     }
 
