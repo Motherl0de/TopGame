227d501ee8391562c62a787055689798d62e1fc5
Anastasia <timmikvo@gmail.com>
2023-11-14 13:37:01 +0200

    Added move for Boat

---
+++ Assets/Scripts/SpawnBoats.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SpawnBoats : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Rigidbody Rb => _rigidbody ??= GetComponent<Rigidbody>();
+    [SerializeField] private Transform _target;
+
+    private void FixedUpdate()
+    {
+        Vector3 toTarget = (_target.position - transform.position).normalized;
+        Vector3 force = Rb.mass * (toTarget * 3f - Rb.velocity) / 1f;
+        Rb.AddForce(force);
+        
+    }
+}
