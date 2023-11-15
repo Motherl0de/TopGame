9479fde604b087e342da3996dbf03c8b1361e994
Anastasia <timmikvo@gmail.com>
2023-11-15 11:42:56 +0200

    Added lookAt Target

--- Assets/Scripts/SpawnBoats.cs
+++ Assets/Scripts/SpawnBoats.cs
@@ -4,12 +4,14 @@ public class SpawnBoats : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private Rigidbody Rb => _rigidbody ??= GetComponent<Rigidbody>();
+    
     [SerializeField] private Transform _target;
 
     private void FixedUpdate()
     {
         Vector3 toTarget = (_target.position - transform.position).normalized;
         Vector3 force = Rb.mass * (toTarget * 3f - Rb.velocity) / 1f;
+        transform.LookAt(_target);
         Rb.AddForce(force);
         
     }
