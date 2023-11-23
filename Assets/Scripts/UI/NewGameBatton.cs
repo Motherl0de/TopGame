5e0635e036952140e0e00e4c34dc8840d06191ab
Alexandr Leto <Alexiell013@gmail.com>
2023-11-23 14:14:28 +0200

    add PanetChenge

---
+++ Assets/Scripts/UI/NewGameBatton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGameBatton : MonoBehaviour
+{
+    public GameObject ChengePlayerPanel;
+
+    public void AnactivatePanet()
+    {
+        ChengePlayerPanel.SetActive(true);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/UI/PlayGame.cs
+++ Assets/Scripts/UI/PlayGame.cs
@@ -5,8 +5,11 @@ namespace T_Scripts.UI
 {
     public class PlayGame : MonoBehaviour
     {
+        public GameObject ChengePlayerPanel;
         public void Play()
         {
+            ChengePlayerPanel.SetActive(false);
+
             SceneManager.LoadScene("GameplayScene");
         }
         public void ExitGame()
