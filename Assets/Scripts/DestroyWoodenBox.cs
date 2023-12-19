119e7d614af8e26b4a6824975743efdd7de541c9
Anastasia <timmikvo@gmail.com>
2023-12-19 17:22:12 +0200

    Create portal

--- Assets/Scripts/DestroyWoodenBox.cs
+++ Assets/Scripts/DestroyWoodenBox.cs
@@ -17,42 +17,32 @@ public sealed class DestroyWoodenBox : MonoBehaviour
         _current = _box[0];
         _next = _box[1];
         _pendingIndex = 2;
-        _portal.Stop();
     }
 
-    // private void Update()
-    // {
-    //     BreakOnClick();
-    // }
-
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.GetComponent<MovePlayer>()) BreakOnClick();
     }
-    // private void OnTriggerEnter(Collider other)
-    // {
-    //     if (other.gameObject.GetComponent<MovePlayer>()) BreakOnClick();
-    // }
 
     private void BreakOnClick()
     {
-        if (Input.GetMouseButtonDown(1) && _pendingIndex <= _box.Length)
+        if (Input.GetKeyDown(KeyCode.Space) && _pendingIndex <= _box.Length)
         {
             _current.SetActive(false);
             if (_next is not null)
             {
                 _next.SetActive(true);
                 Instantiate(_partical, _spawn);
+                if (_next == _box[2].activeInHierarchy) {
+                    _audioPortal.Play();
+                    Instantiate(_portal,new Vector3(-9,10,-10),Quaternion.identity);
+                }
             }
 
             _current = _next;
             _next = _pendingIndex < _box.Length ? _box[_pendingIndex] : null;
             _pendingIndex++;
-            if (_next == _box[2])
-            {
-                _audioPortal.Play();
-                _portal.Play();
-            }
+            
         }
     }
 }
